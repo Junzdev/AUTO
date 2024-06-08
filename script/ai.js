83fe94a2507ae4ea5338334b658729a80a5653b4
Junzdev <109050772+Junzdev@users.noreply.github.com>
2024-06-08 18:56:20 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,7 +1,7 @@
 module.exports.config = {
   name: 'ai',
-  version: '1.0.1', 
-//add more config
+  version: '1.0.1',
+  // Add more config as needed
 };
 
 module.exports.run = async function ({ api, event, args }) {
@@ -11,34 +11,39 @@ module.exports.run = async function ({ api, event, args }) {
   try {
     const info = await api.getUserInfo(senderID);
     const name = info[senderID].name;
-    const r = await axios.post("https://jnai.onrender.com/ai/v2", {
+    const ress = await axios.post("https://jnai.onrender.com/ai/v2", {
       prompt,
       apikey: "jnKey-43p6mGCLjq",
       name,
       id: senderID
     });
-    const { av, result } = r.data;
-    const message = result.replace(/!🐥/g, this.config.name);
-    if (av) {
-    let image = [];
-     if (Array.isArray(av)) {
-        image = await Promise.all(av.map(async (url) => {
-          const re = await axios.get(url, { responseType: 'stream' });
-          re.data.path = `${Date.now()}-${url.split('/').pop()}`;
-          return re.data;
-        }));
+    const { av, result } = ress.data;
+ const res = Array.isArray(result) ? JSON.stringify(result, null, 2) : result.replace(/{pn}/g, "ai");
+ if (av) {
+   let image = [];
+  if (Array.isArray(av)) {
+image = await Promise.all(av.map(async (url) => {
+    const re = await axios.get(url, { responseType: 'stream' });
+ re.data.path = `${Date.now()}-${url.split('/').pop()}`;
+     return re.data;
+     }));
       } else {
-     const re = await axios.get(av, { responseType: 'stream' });
-        image = [re.data];
+  const re = await axios.get(av, { responseType: 'stream' });
+   re.data.path = `${Date.now()}-${av.split('/').pop()}`;
+ image = [re.data];
       }
-      api.sendMessage({ body: message, attachment: image,
-mentions: [{id: senderID, tag: name}]}, threadID, messageID);
+  api.sendMessage({
+   body: res,
+ attachment: image,
+ mentions: [{ id: senderID, tag: name }]
+      }, threadID, messageID);
     } else {
-      api.sendMessage({ body: message,
-mentions: [{id: senderID, tag: name}]}, threadID, messageID);
-    }
+   api.sendMessage({
+  body: res,
+   mentions: [{ id: senderID, tag: name }]
+ }, threadID, messageID);
+ }
   } catch (error) {
-    console.error("Error in AI module:", error);
-    api.sendMessage("something went wrong", threadID);
+    api.sendMessage(error.message, threadID, messageID);
   }
 };

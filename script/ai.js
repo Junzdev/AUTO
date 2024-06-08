1de44c660befb50b2b2ac359dbfe58d42749eb84
Junzdev <109050772+Junzdev@users.noreply.github.com>
2024-06-08 09:00:33 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,22 +1,17 @@
 const axios = require("axios");
 module.exports.config = { name: 'ai', version: '1.0.1' };
-
 module.exports.run = async ({ api, event, args }) => {
   const { threadID, messageID, senderID: id } = event;
   const prompt = args.join(" ");
   try {
-   const { [id]: { name } } = await api.getUserInfo(id);
-    const { data: { av, result } } = await axios.post("https://jn-ai.onrender.com/ai", {
-  prompt, apikey: "jnKey-43p6mGCLjq", name, id
-  });
- const attachments = av ? Array.isArray(av) ? await Promise.all(av.map(async url => {
-      const { data } = await axios.get(url, { responseType: 'stream' });
-  data.path = `${Date.now()}-${url.split('/').pop()}`;
-    return data;
- })) : [{ data: (await axios.get(av, { responseType: 'stream' })).data, path: `${Date.now()}.png` }] : [];
- api.sendMessage({ body: result.replace(/{pn}/g, "ai"), attachment: attachments }, threadID, messageID);
+    const { [id]: { name } } = await api.getUserInfo(id);
+    const { data: { av, result } } = await axios.post("https://jn-ai.onrender.com/ai", { prompt, apikey: "jnKey-43p6mGCLjq", name, id });
+    const r = Array.isArray(result) ? JSON.stringify(result, null, 2) : result.replace(/{pn}/g, prompt);
+    const o = { body: r, mentions: [{ id, tag: name }] };
+    if (av) o.attachment = Array.isArray(av) ? await Promise.all(av.map(async (i) => (await axios.get(i, { responseType: 'stream' })).data)) : (await axios.get(av, { responseType: 'stream' })).data;
+    api.sendMessage(o, threadID, messageID);
   } catch (error) {
-  console.error(error.message);
- api.sendMessage(error.message, threadID);
+    console.error(error.message);
+    api.sendMessage(error.message, threadID);
   }
 };

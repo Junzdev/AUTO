af88a52fd6143b6743f990b784df83f1405be1e5
Junzdev <109050772+Junzdev@users.noreply.github.com>
2024-06-08 18:50:15 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,6 +1,7 @@
 module.exports.config = {
   name: 'ai',
-  version: '1.0.1',
+  version: '1.0.1', 
+//add more config
 };
 
 module.exports.run = async function ({ api, event, args }) {
@@ -10,31 +11,31 @@ module.exports.run = async function ({ api, event, args }) {
   try {
     const info = await api.getUserInfo(senderID);
     const name = info[senderID].name;
-    const r = await axios.post("https://jn-ai.onrender.com/ai", {
+    const r = await axios.post("https://jnai.onrender.com/ai/v2", {
       prompt,
       apikey: "jnKey-43p6mGCLjq",
       name,
       id: senderID
     });
-
     const { av, result } = r.data;
-    const message = result.replace(/{pn}/g, "ai");
-
+    const message = result.replace(/!🐥/g, this.config.name);
     if (av) {
-      let attachments = [];
-      if (Array.isArray(av)) {
-        attachments = await Promise.all(av.map(async (url) => {
+    let image = [];
+     if (Array.isArray(av)) {
+        image = await Promise.all(av.map(async (url) => {
           const re = await axios.get(url, { responseType: 'stream' });
           re.data.path = `${Date.now()}-${url.split('/').pop()}`;
           return re.data;
         }));
       } else {
-        const re = await axios.get(av, { responseType: 'stream' });
-        attachments = [re.data];
+     const re = await axios.get(av, { responseType: 'stream' });
+        image = [re.data];
       }
-      api.sendMessage({ body: message, attachment: attachments }, threadID, messageID);
+      api.sendMessage({ body: message, attachment: image,
+mentions: [{id: senderID, tag: name}]}, threadID, messageID);
     } else {
-      api.sendMessage({ body: message }, threadID, messageID);
+      api.sendMessage({ body: message,
+mentions: [{id: senderID, tag: name}]}, threadID, messageID);
     }
   } catch (error) {
     console.error("Error in AI module:", error);
